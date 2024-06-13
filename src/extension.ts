44090dba681c9ad30b0e02a14e9ba3fc17a9143b
Kundan <kundanims2020@gmail.com>
2024-06-14 00:19:25 +0530

    Second Iteration

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,80 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code!');
-	});
-
-	context.subscriptions.push(disposable);
+class TrieNode {
+    children: { [key: string]: TrieNode } = {};
+    isEndOfWord: boolean = false;
+    words: string[] = [];
 }
 
-// This method is called when your extension is deactivated
+class Trie {
+    root: TrieNode = new TrieNode();
+
+    insert(word: string, originalUrl: string) {
+        let node = this.root;
+        for (const char of word) {
+            if (!(char in node.children)) {
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+            node.words.push(originalUrl);
+        }
+        node.isEndOfWord = true;
+    }
+
+    searchPrefix(prefix: string): string[] {
+        let node = this.root;
+        for (const char of prefix) {
+            if (!(char in node.children)) {
+                return [];
+            }
+            node = node.children[char];
+        }
+        return node.words;
+    }
+}
+
+function createTrie(urls: string[]): Trie {
+    const trie = new Trie();
+    for (const url of urls) {
+        const partAfterQuestionMark = url.split('?', 2)[1] || "";
+        trie.insert(partAfterQuestionMark, url);
+    }
+    return trie;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log("Initializing");
+
+    // Load strings from unique_extracted_parts.json
+    const filePath = path.join(context.extensionPath, 'unique_extracted_parts.json');
+    const urls: string[] = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+    // Create trie from URLs
+    const trie = createTrie(urls);
+
+    const provider1 = vscode.languages.registerCompletionItemProvider('latex', {
+        provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+            const linePrefix = document.lineAt(position).text.substr(0, position.character);
+            const prefixMatch = linePrefix.match(/(\w+)$/);
+            if (!prefixMatch) {
+                return undefined;
+            }
+
+            const prefix = prefixMatch[1];
+            console.log("Searching for prefix:", prefix);
+
+            const suggestions = trie.searchPrefix(prefix);
+            return suggestions.map(url => {
+                const completionItem = new vscode.CompletionItem(url, vscode.CompletionItemKind.Text);
+                completionItem.insertText = url;
+                return completionItem;
+            });
+        }
+    });
+
+    context.subscriptions.push(provider1);
+}
+
 export function deactivate() {}
